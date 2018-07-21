e957053a1d97b3da47d86a070d4bfc1d9fe8aa89
sandeeptile <sandeeptile@gmail.com>
2018-07-21 17:00:58 +0530

    fragment added to button

--- app/src/main/java/com/example/sandy/kotlinfragment/MainActivity.kt
+++ app/src/main/java/com/example/sandy/kotlinfragment/MainActivity.kt
@@ -2,11 +2,59 @@ package com.example.sandy.kotlinfragment
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var fManager=supportFragmentManager
+
+        homebtn.setOnClickListener{
+
+            var tx =fManager.beginTransaction()
+            tx.replace(R.id.frag,HomeFrag())
+            tx.commit()
+        }
+
+        purplebtn.setOnClickListener{
+
+            var tx =fManager.beginTransaction()
+            tx.replace(R.id.frag,PurpleFrag())
+            tx.commit()
+
+        }
+
+        redbtn.setOnClickListener{
+
+            var tx =fManager.beginTransaction()
+            tx.replace(R.id.frag,RedFrag())
+            tx.commit()
+        }
+
+        skybtn.setOnClickListener {
+
+            var tx =fManager.beginTransaction()
+            tx.replace(R.id.frag,SkyblueFrag())
+            tx.commit()
+        }
+
+        yellowbtn.setOnClickListener {
+
+            var tx =fManager.beginTransaction()
+            tx.replace(R.id.frag,YellowFrag())
+            tx.commit()
+        }
+
+        bluebtn.setOnClickListener{
+
+            var tx =fManager.beginTransaction()
+            tx.replace(R.id.frag,BlueFrag())
+            tx.commit()
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/sandy/kotlinfragment/YellowFrag.kt
@@ -0,0 +1,19 @@
+package com.example.sandy.kotlinfragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class YellowFrag:Fragment()
+{
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        var v=inflater.inflate(R.layout.yellowfrag,container,false)
+        return v
+    }
+
+
+}
